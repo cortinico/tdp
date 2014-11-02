cedb122b3007e8a23da1d12f52f750e50fe3a9d4
Nicola Corti <corti.nico@gmail.com>
2014-11-02 14:52:01 +0100

    Completed collision framework

--- TDP/src/Collideable.java
+++ TDP/src/Collideable.java
@@ -1,4 +1,3 @@
-
 public interface Collideable {
 
 	public BoundCircle getBoundCircle();
--- TDP/src/CollisionMediator.java
+++ TDP/src/CollisionMediator.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class CollisionMediator {
 
+	List<Collideable> objects;
+	
+	public CollisionMediator(){
+		objects = new ArrayList<>();
+	}
+	
+	public void addCollideable(Collideable e){
+		objects.add(e);
+	}
+	
+	public void removeCollideable(Collideable e){
+		objects.remove(e);
+	}
+	
+	public void checkCollision(){
+		for (Collideable first: objects){
+			for (Collideable second: objects){
+				if (!first.equals(second)){
+					BoundCircle firstCircle = first.getBoundCircle();
+					BoundCircle secondCircle = second.getBoundCircle();
+					if (overLap(firstCircle, secondCircle))
+						first.collideWith(second);
+				}
+			}
+		}
+	}
+	
+	private boolean overLap(BoundCircle first, BoundCircle second){
+		
+		return false;
+	}
 }
