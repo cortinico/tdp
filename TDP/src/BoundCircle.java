768948b6c9fdd0c8e1648ddeffbbf82ca3cf25ce
Nicola Corti <corti.nico@gmail.com>
2014-11-02 13:00:18 +0100

    Added collideable interface

---
+++ TDP/src/BoundCircle.java
@@ -0,0 +1,24 @@
+public class BoundCircle {
+
+	private int x;
+	private int y;
+	private int radius;
+	
+	public BoundCircle(int x, int y,int radius){
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+}
---
+++ TDP/src/Collideable.java
@@ -0,0 +1,7 @@
+
+public interface Collideable {
+
+	public BoundCircle getBoundCircle();
+	public void collideWith(Collideable c);
+	
+}
