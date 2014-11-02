1e529635c29c63b2b37741925bda465384c2d4eb
Nicola Corti <corti.nico@gmail.com>
2014-11-02 20:17:47 +0100

    Fixed cannon shooting

--- TDP/src/Cannon.java
+++ TDP/src/Cannon.java
@@ -1,15 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Cannon extends GameEntity implements Collideable, Observer {
 
-	// Cannon orientation
 	private int angle = 0;
 	private SpaceShip following;
+
+	private final static int POWER_BAR_SIZE = 10;
+	private final static int POWER_BAR_SPACE = 5;
+	
+	List<PowerBar> firstRing, secondRing, thirdRing;
 		
 	public Cannon(int x, int y) {
 		super(x, y);
 		following = null;
+		firstRing = secondRing = thirdRing = new ArrayList<>(POWER_BAR_SIZE);
+		
+		int relativeAngle = 360 / POWER_BAR_SIZE;
+		
+		for(int i = 0; i < POWER_BAR_SIZE; i++){
+			firstRing.add(new PowerBar(x+POWER_BAR_SPACE, y, relativeAngle * i));
+			secondRing.add(new PowerBar(x+(POWER_BAR_SPACE*2), y, relativeAngle * i));
+			thirdRing.add(new PowerBar(x+(POWER_BAR_SPACE*3), y, relativeAngle * i));
+		}
+			
 	}
 	
 	@Override
@@ -44,8 +60,26 @@ public class Cannon extends GameEntity implements Collideable, Observer {
 			int followingAngle = (this.x -following.getX())/(this.y - following.getY());
 			if (followingAngle > angle)
 				angle++;
-			else
+			else if (followingAngle < angle)
 				angle--;
+			else {
+				// Cannon aligned
+				if (checkPowerBars()){
+					firePlasmaBall();
+				}
+			}
 		}
+	}
+
+	private void firePlasmaBall() {
+		PlasmaBall ball = new PlasmaBall(x, y, angle);
+		GameEnvironment.getInstance().addEntity(ball);
+	}
+
+	private boolean checkPowerBars() {
+		/*	Metodo per controllare se c'e' spazio
+		 * 	fra gli anelli e quindi sparare.
+		 */
+		return false;
 	}
 }
